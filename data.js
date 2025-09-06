ad8c0eaa4156244f4684cead4ea4bf9b005eae5a
komingshyu <komingshyu@yahoo.com.tw>
2025-09-06 19:47:36 +0800

    修改首頁加入其他網頁

---
+++ data.js
@@ -0,0 +1,13 @@
+const linksData = [
+
+  { name: "試算表轉為網頁查詢系統", description: "只要載入試算表XLXS，可以將內容轉為資料查詢網頁，提供資料加密的功能", link: "試算表轉出查詢網頁.html" },
+  { name: "高雄市YouBike即時查詢系統", description: "透過開放資訊即時抓取數據分析呈現目前YouBike狀態", link: "高雄市youbike查詢.html" },
+  { name: "從地圖位置查詢天氣", description: "點選地圖所在地，按下天氣查詢即可顯示目前天氣", link: "可愛天氣查詢系統.html" },
+  { name: "html格式化工具", description: "讓雜亂的html檔案內容格式化整齊", link: "html格式化工具.html" },
+  { name: "台灣新聞直播頻道選台器", description: "直播新聞頻道整合在一起", link: "台灣新聞直播頻道選台器.html" },  
+  { name: "國中生畢業生生涯進路調查表", description: "高雄市畢業生生涯進路調查表", link: "調查表v2.html" },
+  { name: "教師自用的網頁白板", description: "好玩自己測試使用", link: "教師白板更新.html" },
+
+   
+
+];
