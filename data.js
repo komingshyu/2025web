3c4967e2f033700029c2b13426a71a1e899ffcb6
komingshyu <komingshyu@yahoo.com.tw>
2025-09-17 20:21:02 +0800

    增加demo測試檔案

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const linksData = [
 
-  { name: "試算表轉為網頁查詢系統", description: "只要載入試算表XLXS，可以將內容轉為資料查詢網頁，提供資料加密的功能", link: "試算表轉出查詢網頁.html" },
+  { name: "試算表轉為網頁查詢系統", description: "只要載入試算表XLXS，可以將內容轉為資料查詢網頁，提供資料加密的功能<br><a href='網頁測試用.xlsx' >demo檔案：網頁測試用.xlsx</a>", link: "試算表轉出查詢網頁.html" },
   { name: "高雄市YouBike即時查詢系統", description: "透過開放資訊即時抓取數據分析呈現目前YouBike狀態", link: "高雄市youbike查詢.html" },
   { name: "從地圖位置查詢天氣", description: "點選地圖所在地，按下天氣查詢即可顯示目前天氣", link: "可愛天氣查詢系統.html" },
   { name: "html格式化工具", description: "讓雜亂的html檔案內容格式化整齊", link: "html格式化工具.html" },
@@ -14,5 +14,5 @@ const linksData = [
   { name: "瀏覽器IndexedDB 管理系統", description: "瀏覽器IndexedDB 管理系統", link: "瀏覽器IndexedDB 管理系統.html" },
   { name: "網頁安裝檔案生成器", description: "網頁安裝檔案生成器", link: "網頁安裝檔案生成器.html" },  
   { name: "MQTT教學互動加密版V3", description: "利用MQTT來做教學互動", link: "MQTT教學互動加密版V3.html" },
-  { name: "教師座位分配V12版", description: "教師座位分配套印XML版本", link: "教師座位分配V12OK版.html" },
+  { name: "教師座位分配V12版", description: "教師座位分配套印XML版本<br><a href='座位配置圖測試.xml' >demo檔案：座位配置圖測試.xml</a>", link: "教師座位分配V12OK版.html" },
 ];
