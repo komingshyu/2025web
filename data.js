5bb0277aa986b2894f0b052e59dc8954b17e1274
komingshyu <komingshyu@yahoo.com.tw>
2025-12-15 20:42:32 +0800

    增加GAS網頁還原

--- data.js
+++ data.js
@@ -37,5 +37,9 @@ const linksData = [
   { name: "QR Code 線上處理工具", description: "功能說明：QR Code 掃描 及QR Code 生成", link: "QRCode線上處理工具.html" },
   { name: "圖片自動裁切工具", description: "圖片自動裁切工具，對於自製line圖貼切割最方便", link: "圖片自動裁切工具.html" },
   { name: "Google sites平台內嵌網頁內容提取器", description: "將Google協作平台設計的內嵌網頁網頁內容提取為獨立網頁", link: "Google內嵌網頁內容提取器.html" },
+  { name: "Google Apps Script 網頁還原工具", description: "將Google Apps Script 網頁還原為獨立網頁", link: "GAS網頁還原工具.html" },
+  { name: "SQLite 資料庫管理系統V3", description: "可載入sqlite資料庫進行編修管理", link: "SQLite 資料庫管理系統V3.html" },
+
+
 
 ];
